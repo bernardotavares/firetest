d4bb10110c0d2ac23eee8744c103979b8c4dd692
Bernardo Tavares <bernardoptavares@gmail.com>
2018-01-29 10:58:23 +0000

    Initial commit 2

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+var mainText = document.getElementById("mainText");
+var submitBtn = document.getElementById("submitBtn");
+
+function submitClick() {
+
+    var firebase.database().ref();
+
+    firebaseRef.child("Text").set("Some Value");
+    
+}
