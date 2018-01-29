c74c92e6c165ef110527e99302abf5360f2ed30a
Bernardo Tavares <bernardoptavares@gmail.com>
2018-01-29 10:15:38 +0000

    config replace

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Cloud Messaging API Key from https://console.firebase.google.com>'
+  gcmAPIKey: 'AAAA8EjrwX0:APA91bHrb8N6LStdnh33G9nak-hV2W1Oru4mFeY0aUtbAGzC96nJhWJe_1jLbiL0dwP6Klh_Q8W0XWG4TjMGILDxPn0tOz8XC75_3pmDkZJZS5E_Xz3ugTXD4_vbYCjQ3RTJNYiazK05'
 };
